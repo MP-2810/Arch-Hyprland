adf20403da62118f755cb07ab45051827ea56321
MP-2810 <MP-2810@outlook.com>
2026-09-09 23:16:28 +0800

    Add basic Neovim configuration options

---
+++ nvim/lua/core/basic.lua
@@ -0,0 +1,25 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.cursorline = true
+-- vim.opt.colorcolumn = "100"
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 0
+
+vim.opt.autoread = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- 如果查找的内容中不存在大写，则大小写不敏感
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- 不要在查找之后继续高亮匹配结果
+vim.opt.hlsearch = false
+
+vim.opt.showmode = false
+
+-- vim.opt.clipboard = "unnamedplus"
